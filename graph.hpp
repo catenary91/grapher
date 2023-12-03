688b615aab49ffad7b5a067d2912b473e047f8d5
catenary91 <sweetonion135@gmail.com>
2023-12-04 06:30:09 +0900

    final -jy

--- graph.hpp
+++ graph.hpp
@@ -10,49 +10,156 @@ class grapher {
 public:
     grapher(const std::string& f) : p(f) { }
     image draw(double xmin, double xmax, double ymin, double ymax, int height, int width) {
-        image img(height, width);
+            image img(height, width);
 
-        for (int i=0; i<height; i++) {
-            for (int j=0; j<width; j++) {
-                img[i][j] = Pixel(0, 0, 0);
-            }
-        }
+            double interval_x, interval_y;
+            double x = xmin, y = ymin;
 
-        int y_prev = -2; // [-1, height], -2 means invalid
-        // graph
-        for (int i=0; i<width; i++) {
-            double x = (xmax - xmin) / width * i + xmin;
-            double y = p.evaluate(x);
+            int pre_x = 0, pre_y = -1, diff;
+            int pixel_x, pixel_y;
 
-            if (std::isnan(y)) {
-                y_prev = -2;
-                continue;
-            }
+            int smaller = 0, bigger = 0;
 
-            int y_pixel = std::round(height - (y - ymin) / (ymax - ymin) * height);
-            if (y_pixel < 0) {
-                y_prev = -1;
-                continue;
-            }
-            else if (y_pixel >= height) {
-                y_prev = height;
-                continue;
-            }
-            img[y_pixel][i] = Pixel(255, 255, 255);
+            double value;
 
-            // y interpolation
-            if (i != 0 && y_prev != -2 && abs(y_pixel - y_prev)>=2) {
-                int a = std::min(y_prev, y_pixel);
-                int b = std::max(y_prev, y_pixel);
-                for (int j=a+1; j<b; j++) {
-                    img[j][i-1] = Pixel(255, 255, 255);
+            int min_index = 0;
+            double dif_min = fabs(x);
+
+            interval_x = (xmax-xmin)/width;
+            interval_y = (ymax-ymin)/height;
+
+            for (int row = 0; row < width; row++) { //배경색 흰색으로 칠하기
+                for (int col = 0; col < height; col++) {
+                    img[row][col] = Pixel(255,255,255);
                 }
             }
-            if (y_prev == -1 || y_prev == height) y_prev = -2;
-            else y_prev = y_pixel;
+
+
+            for (int col = 0; col < width; col++) { //y축 긋기
+                if (fabs(x) < dif_min) {
+                    dif_min = fabs(x);
+                    min_index = col;
+                }
+
+                x += interval_x;
+            }
+
+            for (int row = 0; row < height; row++) {
+                img[row][min_index] = Pixel(0, 0, 0);
+            }
+
+            min_index = 0;
+            dif_min = fabs(y);
+
+            for (int row = 0; row < height; row++) { //y축 긋기
+                if (fabs(y) < dif_min) {
+                    dif_min = fabs(y);
+                    min_index = row;
+                }
+
+                y += interval_y;
+            }
+
+            for (int col = 0; col < width; col++) {
+                img[min_index][col] = Pixel(0, 0, 0);
+            }
+
+            //그래프 그리기
+            x = xmin;
+            y = ymin;
+
+            for (int pixel_x = 0; pixel_x < width; pixel_x++) {
+                y = ymin;
+                value = p.evaluate(x);
+                // std::printf("x=%lf, y=%lf, isnan=%d\n", x, value, isnan(value));
+                if (std::isnan(value) || std::isinf(value)) { //만약 value가 부적절 하면 그 줄은 스킵
+                    x += interval_x;
+                    pre_x = pixel_x;
+                    pre_y = -1;
+                    continue;
+                } else if (value > ymax) { //주어진 이미지의 크기를 벗어 나고, 그 이전의 좌표가 유효할 때
+                    if (pre_y != -1) {
+                        int i = pre_y;
+                        while(i >= 0) {
+                            img[pre_y - i][pre_x] = Pixel(0,0,0);
+                            i--;
+                        }
+                    }
+
+                    bigger = 1;
+                    smaller = 0;
+                    x += interval_x;
+                    pre_x = pixel_x;
+                    pre_y = -1;
+                    continue;
+                } else if (value < ymin) { //주어진 이미지의 크기를 벗어 나고, 그 이전의 좌표가 유효할 때
+                    if (pre_y != -1) {
+                        int i = pre_y;
+                        while(i < height) {
+                            img[i][pre_x] = Pixel(0,0,0);
+                            i++;
+                        }
+                    }
+
+                    smaller = 1;
+                    bigger = 0;
+                    x += interval_x;
+                    pre_x = pixel_x;
+                    pre_y = -1;
+                    continue;
+                }
+                
+                pixel_y = height - 1;
+                dif_min = fabs(y-value);
+                for (int row = height-1; row >= 0; row--) {
+                    if (fabs(y-value) < dif_min) {
+                        dif_min = fabs(y-value);    
+                        pixel_y = row;
+                    }
+                    y += interval_y;
+                }
+                
+                if (pre_y == -1) { //이전 좌표가 유효하지 않은 경우
+                    diff = 0;
+                } else { //이전 좌표가 유효한 경우 
+                    diff = abs(pixel_y - pre_y);
+                }
+                img[pixel_y][pixel_x] = Pixel(0, 0, 0);
+                
+                if (diff >= 2) { //이전 좌표와 잇는다
+                    if (pre_y < pixel_y) {
+                        for (int i = 1; i < diff; i++) {
+                            img[pre_y + i][pre_x] = Pixel(0,0,0);
+                        }
+                    } else {
+                        for (int i = 1; i < diff; i++) {
+                            img[pre_y - i][pre_x] = Pixel(0,0,0);
+                        }
+                    }
+                }
+
+                if (bigger == 1) { // 이전 좌표가 화면 위쪽에 있는 경우
+                    int i = pixel_y;
+                    while(i >= 0) {
+                        img[i][pre_x] = Pixel(0,0,0);
+                        i--;
+                    }
+                } else if (smaller == 1) { // 이전 좌표가 화면 아래쪽에 있는 경우
+                    int i = pixel_y + 1;
+                    while(i < height) {
+                        img[i][pre_x] = Pixel(0,0,0);
+                        i++;
+                    }
+                }
+
+                x += interval_x;
+                pre_x = pixel_x;
+                pre_y = pixel_y;
+                bigger = 0;
+                smaller = 0;
+            }
+            return img;
         }
-        return img;
-    }
 private:
     parser p;
 };
